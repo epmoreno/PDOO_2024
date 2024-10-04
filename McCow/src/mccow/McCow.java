87cfa2aa516cc74a11ee831ff2cafe4782ba18f5
epmoreno <epmoreno@correo.ugr.es>
2024-10-04 19:21:50 +0200

    Configuracion corregida

---
+++ McCow/src/mccow/McCow.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package mccow;
+
+/**
+ *
+ * @author enrique
+ */
+public class McCow {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
