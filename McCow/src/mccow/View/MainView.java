4615f756bccc832635c89001f9b8eae152c0a2ea
epmoreno <epmoreno@correo.ugr.es>
2024-11-19 15:45:52 +0100

    Release 3

--- McCow/src/mccow/View/MainView.java
+++ McCow/src/mccow/View/MainView.java
@@ -11,14 +11,14 @@ import java.util.List;
  *
  * @author enrique
  */
-public class prueba extends javax.swing.JFrame {
+public class MainView extends javax.swing.JFrame {
     List<MesaView> mesasView1 = new ArrayList<>();
     List<MesaView> mesasView2 = new ArrayList<>();
     MesaView mesasView3 = new MesaView();
     /**
      * Creates new form prueba
      */
-    public prueba() {
+    public MainView() {
         initComponents();
         
         for(int i = 0;i<6;i++){
@@ -87,20 +87,21 @@ public class prueba extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(prueba.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(MainView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(prueba.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(MainView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(prueba.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(MainView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(prueba.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(MainView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new prueba().setVisible(true);
+                new MainView().setVisible(true);
             }
         });
     }
